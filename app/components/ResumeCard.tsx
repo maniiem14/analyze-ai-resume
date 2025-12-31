49d7fd5b8a9d17332dce173f59845ee7619c10ea
Lalmani Kumar <lalmani.k@jebitech.com>
2025-12-31 17:47:02 +0530

    Fetch real resumes on homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
+import { resume } from "react-dom/server";
 
 const ResumeCard = ({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
   resume: Resume;
 }) => {
+  const { fs } = usePuterStore();
+  const [ resumeUrl, setResumeUrl ] = useState('')
+
+
+     useEffect(() => {
+      const loadResume = async () => {
+        const blob = await fs.read(imagePath);
+        if(!blob) return;
+        let url = URL.createObjectURL(blob);
+        setResumeUrl(url)
+      }
+
+      loadResume();
+    }, [imagePath]);
+
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -14,9 +32,11 @@ const ResumeCard = ({
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="font-bold !text-black break-words">{companyName}</h2>
+          { companyName && <h2 className="font-bold !text-black break-words">{companyName}</h2> }
 
-          <h3 className="text-gray-500 text-lg break-words">{jobTitle}</h3>
+         { jobTitle && <h3 className="text-gray-500 text-lg break-words">{jobTitle}</h3> }
+
+         { companyName && jobTitle && <h2 className="!text-black font-bold" >Resume</h2> }
         </div>
 
         <div className="shrink-0">
@@ -24,15 +44,19 @@ const ResumeCard = ({
         </div>
       </div>
 
+      {resumeUrl && (
+        
+
       <div className="gradient-border animate-in fade-in duration-1000">
         <div className="w-full h-full">
           <img
-            src={imagePath}
+            src={resumeUrl}
             alt="resume"
             className="w-full h-[350px] max-sm:h-50 object-cover object-top"
           />
         </div>
       </div>
+      )}
     </Link>
   );
 };
